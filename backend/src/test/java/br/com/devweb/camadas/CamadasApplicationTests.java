36b1139da4ef16f5af0726df22afb8f563c5eeca
Matheus Soares <matheus.sousoa.dev@gmail.com>
2024-05-07 07:29:26 -0300

    first backend commit

---
+++ backend/src/test/java/br/com/devweb/camadas/CamadasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devweb.camadas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamadasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
